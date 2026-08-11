0892817caa6ca028a8cd6e6dff39a80c0c31cc5b
soundbier <soundbier@gmail.com>
2026-08-11 08:35:24 +0200

    Anpassungen Ui

--- sw2.js
+++ sw2.js
@@ -1,7 +1,7 @@
 // =========================================================================
-// UPDATE-STEUERUNG: v1.1.9.2
+// UPDATE-STEUERUNG: v1.1.9.3
 // =========================================================================
-const CACHE_NAME = 'revisions-tool-v1.1.9.2';
+const CACHE_NAME = 'revisions-tool-v1.1.9.3';
 
 const ASSETS_TO_CACHE = [
     'index.html',
